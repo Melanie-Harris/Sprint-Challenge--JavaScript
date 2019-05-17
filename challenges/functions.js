4c7504d5390a7a2f76c415f5018b2c0d462fbbba
Melanie Harris <mell.j.harris@gmail.com>
2019-05-17 10:19:02 -0700

    prototypes and class conversion complete. last step is to complete the functions assignment. working on this section now

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,18 +7,35 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+// Const hobbies = ["Coding", "Yoga", "Art"];
+// Function firstItem(arr, callback) {
+//   callback(arr[3]);
+// }
+// Const Item(item, function (firstItem) {
+//   console.log(firstItem);
+// });
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+const add = (num1, num2) => {
+  return num1 + num2;
+}
 
+const multiply = (num1, num2) => {
+  return num1 * num2;
+}
+
+const greeting = (first_name, last_name) => {
+ return `Hello ${first_name} ${last_name}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+ consume(2,2,add); // 4
+ consume(10,16,multiply); // 160
+ consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
